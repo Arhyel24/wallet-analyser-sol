8f38e87c9d70910ec3935cb80570588cef7bb7fd
Enoch <arhyelphilip024@gmail.com>
2024-12-22 13:10:43 +0100

    finalysing

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default async function GET(req: NextRequest, res: NextResponse) {
-    const request = await req.json();
-    console.log(request);
+export default async function GET(req: NextRequest) {
+  const request = await req.json();
+  console.log(request);
 
-    return NextResponse.json("", {status: 200});
+  return NextResponse.json("", { status: 200 });
 }
